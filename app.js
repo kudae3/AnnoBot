430097589a476a616e1f37280f6231d8fd97f67a
Kudae Sithu <kudaesithu2@gmail.com>
2026-01-20 15:01:16 +0630

    refactor: migrate to ES6 modules and simplify server setup

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+const PORT = 3000;
 
-var app = express();
+app.get('/', (req, res) => {
+  res.send('✅ Server is running');
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
